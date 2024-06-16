5b3d0601268835339b753c6de179917e44a692d6
Chengyu Huang <huangchengyu16@gmail.com>
2024-06-16 19:18:58 +0000

    add vllm

---
+++ src/inference/basic_inference.py
@@ -0,0 +1,39 @@
+#%%
+### test file 
+
+import transformers
+import torch
+
+#%%
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+cache_dir = "/root/data/hf_cache"
+hf_access_token = input("Please enter the hf key: ") ## hf access token 
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16}, ## load with bf16
+    device_map="auto",
+    #cache_dir = cache_dir,
+    token = hf_access_token
+)
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+)
+
+print(outputs[0]["generated_text"][-1])
+
---
+++ src/libs/utils.py
@@ -0,0 +1,18 @@
+## utils 
+#%%
+from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
+
+#%%
+
+def donload_hf_model(REPO_ID,save_location):
+    # REPO_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
+    # save_location = '/root/data/hf_cache/llama-3-8B-Instruct'
+    hf_token = input("huggingface token:")
+    snapshot_download(repo_id=REPO_ID,
+                    local_dir=save_location,
+                    token=hf_token)
+    
+    
+    
+#%%
