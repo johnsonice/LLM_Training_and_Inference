5071638def0cf62c9f80de302617393349b81617
Chengyu Huang <huangchengyu16@gmail.com>
2024-09-01 21:15:38 +0000

    update test

--- src/inference/unit_test/openai_api_unit_test.py
+++ src/inference/unit_test/openai_api_unit_test.py
@@ -1,7 +1,8 @@
 #%%
 import openai,os
 import timeit
-from dotenv import load_dotenv, find_dotenv
+from functools import partial
+from dotenv import load_dotenv
 env_path = '../../../.env'
 load_dotenv(dotenv_path=env_path)
 
@@ -11,10 +12,13 @@ client = openai.Client(
     api_key=os.getenv("Netmind_AIP_KEY")
     )
 
-def get_llm_text_response(user_message,client,max_tokens=4096):
+def get_llm_text_response(user_message,
+                          client,
+                          model_name="meta-llama-Meta-Llama-3.1-8B-Instruct",
+                          max_tokens=4096):
     # Chat completion
     response = client.chat.completions.create(
-        model="meta-llama-Meta-Llama-3.1-8B-Instruct", # "llama3-8b"  or "llama3-70b"
+        model=model_name, 
         messages=[
             {"role": "system", "content": "You are a helpful AI assistant"},
             {"role": "user", "content": user_message},
@@ -23,19 +27,27 @@ def get_llm_text_response(user_message,client,max_tokens=4096):
         max_tokens=max_tokens,
     )
     return response.choices[0].message.content
+#%%
+# Running the timeit 5 times
+test_models = ["meta-llama-Meta-Llama-3.1-8B-Instruct",
+               "meta-llama-Meta-Llama-3.1-70B-Instruct"]
+for m in test_models:
+    stmt = partial(get_llm_text_response,'please write a news article that is 1000 words long.',
+                client,
+                m,
+                4096)
+    execution_time = timeit.repeat(stmt=stmt, repeat=1, number=5)
+    print("{} Time used: {:.2f}".format(m,sum(execution_time)))
 
+#%%
 user_messages = [
                 'please write a news article that is 2000 words long.',
                 'Create a simple game with python code',
                 'Explain the difference between quantum physics and Newtonian physics.',
                 'Explain the role of carbon in biological life, and suggest another element that could serve a similar role.',
-                 ]
+                ]
 for idx,u_m in enumerate(user_messages):
     print('Answer {}'.format(idx))
-    print(get_llm_text_response(u_m,client,4096))
-    
-#%%
-
-# # Running the timeit 10 times
-# execution_time = timeit.repeat(stmt=get_llm_text_response, repeat=10, number=1)
-# print("Time used: {:.2f}".format(sum(execution_time)))
+    print(get_llm_text_response(u_m,client,
+                                "meta-llama-Meta-Llama-3.1-8B-Instruct",
+                                4096))
